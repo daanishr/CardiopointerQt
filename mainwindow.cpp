977b7cdeb4ac83436a43eb1e34e28a06740b5cb4
Daanish Rijhwani <zcabdri@ucl.ac.uk>
2014-01-10 15:05:09 +0530

    UI changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtCore>
+#include <QtGui>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,35 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    //capture model
+    QMessageBox::StandardButton answer;
+    answer = QMessageBox::question(this,"Capture Model"," Model Captured\n Do you want to save?",QMessageBox::No | QMessageBox::Yes);
+
+    if(answer == QMessageBox::Yes){
+        QMessageBox::information(this,"Capture Model","File saved successfully");
+
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    //capture cross section
+    QMessageBox::StandardButton answer;
+    answer = QMessageBox::question(this,"Capture Crossection"," Cross section captured\n Do you want to save?",QMessageBox::No | QMessageBox::Yes);
+    if(answer == QMessageBox::Yes){
+        QMessageBox::information(this,"Cross section capture","File saved successfully");
+
+    }
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    //reset model
+     QMessageBox::information(this,"Reset","Model Reset successfull");
 }
